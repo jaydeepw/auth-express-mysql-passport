8a38ca6201835272f1bf01a0d8a8f01113a548f2
Jaydeep <jwagh@whileone.in>
2024-05-28 15:15:06 +0530

    Working JWT with passport

--- index.js
+++ index.js
@@ -1,253 +1,95 @@
-const passport=require('passport');
-const LocalStrategy=require('passport-local').Strategy;
-const express = require('express');
-const app = express();
-const bodyParser = require("body-parser");
-const mysql = require('mysql');
-const crypto=require('crypto');
-var session = require('express-session');
-var MySQLStore = require('express-mysql-session')(session);
+// file: index.js
 
+var _ = require("lodash");
+var express = require("express");
+var bodyParser = require("body-parser");
+var jwt = require('jsonwebtoken');
 
-/*Mysql Express Session*/
+var passport = require("passport");
+var passportJWT = require("passport-jwt");
 
-app.use(session({
-	key: 'session_cookie_name',
-	secret: 'session_cookie_secret',
-	store: new MySQLStore({
-        host:'localhost',
-        port:3306,
-        user:'root',
-        database:'cookie_user'
-    }),
-	resave: false,
-    saveUninitialized: false,
-    cookie:{
-        maxAge:1000*60*60*24,
-       
-    }
-}));
+var ExtractJwt = passportJWT.ExtractJwt;
+var JwtStrategy = passportJWT.Strategy;
 
+var users = [
+  {
+    id: 1,
+    name: 'jonathanmh',
+    password: 'jonny1'
+  },
+  {
+    id: 2,
+    name: 'test',
+    password: 'test'
+  }
+];
+
+var jwtOptions = {}
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeader();
+jwtOptions.secretOrKey = 'tasmanianDevil';
+
+var strategy = new JwtStrategy(jwtOptions, function(jwt_payload, next) {
+  console.log('payload received', jwt_payload);
+  // usually this would be a database call:
+  var user = users[_.findIndex(users, {id: jwt_payload.id})];
+  if (user) {
+    next(null, user);
+  } else {
+    next(null, false);
+  }
+});
+
+passport.use(strategy);
+
+var app = express();
 app.use(passport.initialize());
-app.use(passport.session());
-app.use(bodyParser.json());
+
+// parse application/x-www-form-urlencoded
+// for easier testing with Postman or plain HTML forms
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(express.static('public'));
-app.set("view engine", "ejs");
 
+// parse application/json
+app.use(bodyParser.json())
 
-/*Mysql Connection*/
-
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    database: "user",
-    multipleStatements: true
-  });
-  connection.connect((err) => {
-    if (!err) {
-      console.log("Connected");
-    } else {
-      console.log("Conection Failed");
-    }
-  });
- 
-
-const customFields={
-    usernameField:'uname',
-    passwordField:'pw',
-};
-
-
-/*Passport JS*/
-const verifyCallback=(username,password,done)=>{
-   
-     connection.query('SELECT * FROM users WHERE username = ? ', [username], function(error, results, fields) {
-        if (error) 
-            return done(error);
-
-        if(results.length==0)
-        {
-            return done(null,false);
-        }
-        const isValid=validPassword(password,results[0].hash,results[0].salt);
-        user={id:results[0].id,username:results[0].username,hash:results[0].hash,salt:results[0].salt};
-        if(isValid)
-        {
-            return done(null,user);
-        }
-        else{
-            return done(null,false);
-        }
-    });
-}
-
-const strategy=new LocalStrategy(customFields,verifyCallback);
-passport.use(strategy);
-
-
-passport.serializeUser((user,done)=>{
-    console.log("inside serialize");
-    done(null,user.id)
+app.get("/", function(req, res) {
+  res.json({message: "Express is up!"});
 });
 
-passport.deserializeUser(function(userId,done){
-    console.log('deserializeUser'+ userId);
-    connection.query('SELECT * FROM users where id = ?',[userId], function(error, results) {
-            done(null, results[0]);    
-    });
+app.post("/login", function(req, res) {
+  if(req.body.name && req.body.password){
+    var name = req.body.name;
+    var password = req.body.password;
+  }
+  // usually this would be a database call:
+  var user = users[_.findIndex(users, {name: name})];
+  if( ! user ){
+    res.status(401).json({message:"no such user found"});
+  }
+
+  if(user.password === req.body.password) {
+    // from now on we'll identify the user by the id and the id is the only personalized value that goes into our token
+    var payload = {id: user.id};
+    var token = jwt.sign(payload, jwtOptions.secretOrKey);
+    res.json({message: "ok", token: token});
+  } else {
+    res.status(401).json({message:"passwords did not match"});
+  }
 });
 
+app.get("/secret", passport.authenticate('jwt', { session: false }), function(req, res){
+  res.json({message: "Success! You can not see this without a token"});
+});
 
-
-/*middleware*/
-function validPassword(password,hash,salt)
-{
-    var hashVerify=crypto.pbkdf2Sync(password,salt,10000,60,'sha512').toString('hex');
-    return hash === hashVerify;
-}
-
-function genPassword(password)
-{
-    var salt=crypto.randomBytes(32).toString('hex');
-    var genhash=crypto.pbkdf2Sync(password,salt,10000,60,'sha512').toString('hex');
-    return {salt:salt,hash:genhash};
-}
-
-
- function isAuth(req,res,next)
-{
-    if(req.isAuthenticated())
-    {
-        next();
-    }
-    else
-    {
-        res.redirect('/notAuthorized');
-    }
-}
-
-
-function isAdmin(req,res,next)
-{
-    if(req.isAuthenticated() && req.user.isAdmin==1)
-    {
-        next();
-    }
-    else
-    {
-        res.redirect('/notAuthorizedAdmin');
-    }   
-}
-
-function userExists(req,res,next)
-{
-    connection.query('Select * from users where username=? ', [req.body.uname], function(error, results, fields) {
-        if (error) 
-            {
-                console.log("Error");
-            }
-       else if(results.length>0)
-         {
-            res.redirect('/userAlreadyExists')
-        }
-        else
-        {
-            next();
-        }
-       
-    });
-}
-
-
-app.use((req,res,next)=>{
-    console.log(req.session);
-    console.log(req.user);
+app.get("/secretDebug",
+  function(req, res, next){
+    console.log(req.get('Authorization'));
     next();
-});
-
-/*routes*/
-app.get('/', (req, res, next) => {
-    res.send('<h1>Home</h1><p>Please <a href="/register">register</a></p>');
-});
-
-app.get('/login', (req, res, next) => {
-        res.render('login')
-});
-app.get('/logout', (req, res, next) => {
-    req.logout(); //delets the user from the session
-    res.redirect('/protected-route');
-});
-app.get('/login-success', (req, res, next) => {
-    res.send('<p>You successfully logged in. --> <a href="/protected-route">Go to protected route</a></p>');
-});
-
-app.get('/login-failure', (req, res, next) => {
-    res.send('You entered the wrong password.');
-});
-
-
-app.get('/register', (req, res, next) => {
-    console.log("Inside get");
-    res.render('register')
-    
-});
-
-app.post('/register',userExists,(req,res,next)=>{
-    console.log("Inside post");
-    console.log(req.body.pw);
-    const saltHash=genPassword(req.body.pw);
-    console.log(saltHash);
-    const salt=saltHash.salt;
-    const hash=saltHash.hash;
-
-    connection.query('Insert into users(username,hash,salt,isAdmin) values(?,?,?,0) ', [req.body.uname,hash,salt], function(error, results, fields) {
-        if (error) 
-            {
-                console.log("Error");
-            }
-        else
-        {
-            console.log("Successfully Entered");
-        }
-       
-    });
-
-    res.redirect('/login');
-});
-
-app.post('/login',passport.authenticate('local',{failureRedirect:'/login-failure',successRedirect:'/login-success'}));
-
-app.get('/protected-route',isAuth,(req, res, next) => {
- 
-    res.send('<h1>You are authenticated</h1><p><a href="/logout">Logout and reload</a></p>');
-});
-
-app.get('/admin-route',isAdmin,(req, res, next) => {
- 
-    res.send('<h1>You are admin</h1><p><a href="/logout">Logout and reload</a></p>');
-
+  }, function(req, res){
+    res.json("debugging");
 });
 
 app.listen(3000, function() {
-    console.log('App listening on port 8080!')
-  });
-
-
-  app.get('/notAuthorized', (req, res, next) => {
-    console.log("Inside get");
-    res.send('<h1>You are not authorized to view the resource </h1><p><a href="/login">Retry Login</a></p>');
-    
-});
-app.get('/notAuthorizedAdmin', (req, res, next) => {
-    console.log("Inside get");
-    res.send('<h1>You are not authorized to view the resource as you are not the admin of the page  </h1><p><a href="/login">Retry to Login as admin</a></p>');
-    
-});
-app.get('/userAlreadyExists', (req, res, next) => {
-    console.log("Inside get");
-    res.send('<h1>Sorry This username is taken </h1><p><a href="/register">Register with different username</a></p>');
-    
+  console.log("Express running");
 });
